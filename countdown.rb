e2e3d95b9a0e329aea0faf016e88f8d88679f8b0
Patrick McCombs <pa.mccombs1@gmail.com>
2018-05-16 04:43:42 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,13 +4,10 @@ def countdown(number)
   while number > 0
   puts "#{number} SECOND(S)!"
   number -= 1
- 
-  
-  
-  
 end
   "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
+def countdown_with_sleep(number)
+  while
 end
